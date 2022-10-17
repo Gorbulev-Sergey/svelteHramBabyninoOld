571c83086e65385707d3ea7914a0487fa56de5fe
Горбулёв Сергей <gorbulevsv@yandex.ru>
2022-10-18 00:31:50 +0300

    7

--- src/routes/admin/schedule/+page.svelte
+++ src/routes/admin/schedule/+page.svelte
@@ -1,9 +1,34 @@
 <script>
+	// @ts-nocheck
+
+	import ComponentPageTitle from '$lib/components/ComponentPageTitle.svelte';
+	import { db } from '$lib/scripts/firebase';
+	import { push, ref, set, update } from 'firebase/database';
+	import FildDate from '../../schedule/FildDate';
 	import Month from '../../schedule/Month';
 	import ComponentMonthEdit from './ComponentMonthEdit.svelte';
 
 	let month = new Month(10, 2022);
 </script>
 
-<h4 class="mb-3">Расписание</h4>
-<ComponentMonthEdit {month} />
+<ComponentPageTitle title="Редактировать расписание">
+	<div slot="navigation">
+		<button
+			class="btn btn-light"
+			on:click={async () => {
+				month.fildsDayAll.forEach((fDate) => {
+					let fildDate = fDate;
+					console.log(fDate);
+					fDate.fildsTime.forEach((fTime) => {
+						if (fTime.event != '') fildDate.fildsTime.push(fTime);
+					});
+					if (FildDate.length > 0) month.fildsDayNotEmpty.push(fildDate);
+				});
+				console.log(month.fildsDayNotEmpty);
+				set(ref(db, `schedule/${month.monthName()}`), month.fildsDayNotEmpty);
+			}}>Сохранить</button
+		>
+	</div>
+</ComponentPageTitle>
+
+<ComponentMonthEdit bind:month />
