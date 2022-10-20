74214c979abf307c8c0010b9780c1eb5644b069a
Горбулёв Сергей <gorbulevsv@yandex.ru>
2022-10-20 22:28:35 +0300

    1

--- src/routes/schedule/ComponentMonth.svelte
+++ src/routes/schedule/ComponentMonth.svelte
@@ -8,7 +8,10 @@
 
 {#if month.fildsDayNotEmpty != null}
 	<div class="rounded bg-white">
-		<div class="pt-1" style="display: grid; grid-template-columns: .12fr 1fr">
+		<div class="ms-4 py-2">
+			<b>Расписание на {month.monthName().toUpperCase()} {month.year} года</b>
+		</div>
+		<div class="pt-1" style="display: grid; grid-template-columns: .12fr 1fr; min-height: 2.3em">
 			<div class="text-center">
 				<span class="badge bg-dark text-dark bg-opacity-10">Дата</span>
 			</div>
