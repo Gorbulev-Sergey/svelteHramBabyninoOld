655a0aedb841b711dba1bf80b47a25ac62d56b4f
Горбулёв Сергей <gorbulevsv@yandex.ru>
2022-10-20 16:46:41 +0300

    1

--- src/lib/components/ComponentNavigation.svelte
+++ src/lib/components/ComponentNavigation.svelte
@@ -17,20 +17,22 @@
 	];
 </script>
 
-<div class="fixed-top bg-light py-1 border-bottom">
+<div class="fixed-top bg-white py-2">
 	<div class="container-fluid d-flex justify-content-between align-items-center">
 		<div class="flex-grow-1 d-flex justify-content-start align-items-center">
-			<button class="btn btn-light text-uppercase me-2" on:click={() => goto('/')}
+			<button class="btn btn-light bg-white border-0 text-uppercase me-2" on:click={() => goto('/')}
 				><b>ХРАМ</b></button
 			>
 			<div>
 				{#each routes as item}
-					<a class="btn btn-light" href={item.url}>{item.title}</a>
+					<a class="btn btn-light bg-white border-0 me-1" href={item.url}>{item.title}</a>
 				{/each}
 			</div>
 		</div>
 		<div>
-			<a class="btn btn-light me-1" href="/admin/schedule">Редактировать расписание</a>
+			<a class="btn btn-light bg-white border-0 me-1" href="/admin/schedule"
+				>Редактировать расписание</a
+			>
 		</div>
 	</div>
 </div>
