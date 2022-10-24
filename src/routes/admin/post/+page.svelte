dc5e6985f88597c3b369daf8ecae327f62e9c6c4
Горбулёв Сергей <gorbulevsv@yandex.ru>
2022-10-24 11:26:38 +0300

    12

--- src/routes/admin/post/+page.svelte
+++ src/routes/admin/post/+page.svelte
@@ -22,92 +22,88 @@
 	</div>
 </ComponentPageTitle>
 
-<ul class="nav nav-pills mb-3">
-	<a class="btn btn-light border-0 me-1 active" data-bs-toggle="pill" href="#cover">Обложка</a>
-	<a class="btn btn-light border-0" data-bs-toggle="pill" href="#content">Содержимое</a>
-</ul>
+<div class="rounded p-3 {!post.inverted ? 'bg-white text-dark' : 'bg-dark text-light'}">
+	<ul class="nav nav-pills mb-3">
+		<a class="btn btn-light border-0 me-1 active" data-bs-toggle="pill" href="#cover">Обложка</a>
+		<a class="btn btn-light border-0" data-bs-toggle="pill" href="#content">Содержимое</a>
+	</ul>
 
-<div class="tab-content">
-	<div class="tab-pane show" id="cover">
-		<div class="row">
-			<div class="col-md-8">
-				<input class="form-control border-0 mb-3" placeholder="заголовок" bind:value={post.title} />
-				<textarea
-					class="form-control border-0 mb-3"
-					placeholder="краткое описание"
-					style="min-height: 10em;"
-					bind:value={post.description}
-				/>
-				<div class="d-flex justify-content-between">
-					<div>
-						<input
-							id="published"
-							type="checkbox"
-							class="bg-dark text-light"
-							bind:checked={post.published}
-						/>
-						<label for="published">опубликовать</label>
-					</div>
-					<div>
-						<input
-							id="inverted"
-							type="checkbox"
-							class="bg-dark text-light"
-							bind:checked={post.inverted}
-						/>
-						<label for="inverted">инвертировать цвета</label>
-					</div>
-					<div>
-						<input
-							id="pinned"
-							type="checkbox"
-							class="bg-dark text-light"
-							bind:checked={post.pinned}
-						/>
-						<label for="pinned">закрепить наверху</label>
-					</div>
-				</div>
-			</div>
-			<div class="col-md-4">
-				<div class="form-group mb-3">
+	<div class="tab-content">
+		<div class="tab-pane show" id="cover">
+			<div class="row">
+				<div class="col-md-8">
 					<input
-						class="form-control border-0 mb-3"
-						placeholder="обложка (url фото)"
-						bind:value={post.cover.image}
+						class="form-control border-0 mb-3
+						{!post.inverted ? 'bg-light text-dark bg-opacity-75' : 'bg-secondary text-light bg-opacity-10'}"
+						placeholder="заголовок"
+						bind:value={post.title}
 					/>
-					{#if post.cover.image}
-						<img class="card-img rounded" src={post.cover.image} alt="" />
-					{/if}
-				</div>
-				<div class="form-group mb-3">
-					<input
-						class="form-control border-0 mb-3"
-						placeholder="обложка (url видео)"
-						bind:value={post.cover.video}
+					<textarea
+						class="form-control border-0 mb-3
+						{!post.inverted ? 'bg-light text-dark bg-opacity-75' : 'bg-secondary text-light bg-opacity-10'}"
+						placeholder="краткое описание"
+						style="min-height: 10em;"
+						bind:value={post.description}
 					/>
-					{#if post.cover.video}
-						<div class="ratio ratio-16x9 rounded overflow-hidden mt-3">
-							<!-- svelte-ignore a11y-missing-attribute -->
-							<iframe
-								class="embed-responsive-item"
-								src="{post.cover.video
-									.toString()
-									.replace('https://youtu.be', 'https://youtube.com/embed/')}?rel=0"
-								frameborder="0"
-								allowfullscreen
-							/>
+					<div class="d-flex justify-content-between">
+						<div>
+							<input id="published" type="checkbox" bind:checked={post.published} />
+							<label for="published">опубликовать</label>
 						</div>
-					{/if}
+						<div>
+							<input id="inverted" type="checkbox" bind:checked={post.inverted} />
+							<label for="inverted">инвертировать цвета</label>
+						</div>
+						<div>
+							<input id="pinned" type="checkbox" bind:checked={post.pinned} />
+							<label for="pinned">закрепить наверху</label>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-4">
+					<div class="form-group mb-3">
+						<input
+							class="form-control border-0 mb-3
+							{!post.inverted ? 'bg-light text-dark bg-opacity-75' : 'bg-secondary text-light bg-opacity-10'}"
+							placeholder="обложка (url фото)"
+							bind:value={post.cover.image}
+						/>
+						{#if post.cover.image}
+							<img class="card-img rounded" src={post.cover.image} alt="" />
+						{/if}
+					</div>
+					<div class="form-group mb-3">
+						<input
+							class="form-control border-0 mb-3
+							{!post.inverted ? 'bg-light text-dark bg-opacity-75' : 'bg-secondary text-light bg-opacity-10'}"
+							placeholder="обложка (url видео)"
+							bind:value={post.cover.video}
+						/>
+						{#if post.cover.video}
+							<div class="ratio ratio-16x9 rounded overflow-hidden mt-3">
+								<!-- svelte-ignore a11y-missing-attribute -->
+								<iframe
+									class="embed-responsive-item"
+									src="{post.cover.video
+										.toString()
+										.replace('https://youtu.be', 'https://youtube.com/embed/')}?rel=0"
+									frameborder="0"
+									allowfullscreen
+								/>
+							</div>
+						{/if}
+					</div>
 				</div>
 			</div>
 		</div>
-	</div>
-	<div class="tab-pane" id="content">
-		<textarea
-			class="form-control border-0"
-			style="min-height: 20em;"
-			placeholder="содержимое"
-			bind:value={post.content}
-		/>
+		<div class="tab-pane" id="content">
+			<textarea
+				class="form-control border-0
+				{!post.inverted ? 'bg-light text-dark bg-opacity-75' : 'bg-secondary text-light bg-opacity-10'}"
+				style="min-height: 20em;"
+				placeholder="содержимое"
+				bind:value={post.content}
+			/>
+		</div>
 	</div>
 </div>
