ccde0b1ac2f140c6432ae8b659bb27352b9b3fa0
Горбулёв Сергей <gorbulevsv@yandex.ru>
2022-10-24 23:01:05 +0300

    2

--- src/routes/admin/tag/+page.svelte
+++ src/routes/admin/tag/+page.svelte
@@ -20,40 +20,45 @@
 
 <ComponentPageTitle title="Теги" />
 
-<div class="bg-white rounded p-3 mb-3">
-	<h5>Добавить новый тег</h5>
-	<div class="input-group">
-		<input class="form-control" placeholder="название" bind:value={tag.tag.name} />
-		<input class="form-control" placeholder="описание" bind:value={tag.tag.description} />
-		<button
-			class="btn btn-dark"
-			on:click={async () => {
-				if (tag.tag.name != '') {
-					tag.tag.name = tag.tag.name.toLocaleLowerCase();
-					tag.tag.description =
-						tag.tag.description[0].toLocaleUpperCase() + tag.tag.description.slice(1);
-					update(ref(db, `tags/${tag.uid}`), tag.tag);
-					tag = { uid: '', tag: new Tag() };
-				}
-			}}>Сохранить</button
-		>
-	</div>
-</div>
-
-<div class="bg-white rounded p-3">
-	<h5>Доступные теги</h5>
-	{#each Object.entries(tags) as [uid, item]}
-		<div class="btn-group btn-group-sm me-2">
-			<div class="bg-light text-dark py-1 px-2 rounded-start">{item.name}</div>
-			<div
-				class="btn btn-dark"
-				on:click={() => {
-					tag = { uid: uid, tag: item };
-				}}
-			>
-				<i class="fa-solid fa-pencil" />
+<div class="row bg-white rounded p-3 mb-3">
+	<div class="col">
+		<h5>Доступные теги</h5>
+		{#each Object.entries(tags) as [uid, item]}
+			<div class="btn-group btn-group-sm me-2">
+				<div class="bg-light text-dark py-1 px-2 rounded-start">{item.name}</div>
+				<div
+					class="btn btn-dark"
+					on:click={() => {
+						tag = { uid: uid, tag: item };
+					}}
+				>
+					<i class="fa-solid fa-pencil" />
+				</div>
+				<div class="btn btn-dark"><i class="fa-solid fa-trash text-danger" /></div>
 			</div>
-			<div class="btn btn-dark"><i class="fa-solid fa-trash text-danger" /></div>
+		{/each}
+	</div>
+	<div class="col">
+		<h5>Добавить новый тег</h5>
+		<div class="input-group">
+			<input class="form-control" placeholder="название" bind:value={tag.tag.name} />
+			<input class="form-control" placeholder="описание" bind:value={tag.tag.description} />
+			<button
+				class="btn btn-dark"
+				on:click={async () => {
+					if (tag.tag.name != '') {
+						tag.tag.name = tag.tag.name.toLocaleLowerCase();
+						if (!tag.tag.description)
+							tag.tag.description = tag.tag?.name[0].toLocaleUpperCase() + tag.tag?.name.slice(1);
+						else
+							tag.tag.description =
+								tag.tag?.description[0].toLocaleUpperCase() + tag.tag?.description.slice(1);
+						if (!tag.uid) push(ref(db, 'tags/'), tag.tag);
+						else update(ref(db, `tags/${tag.uid}`), tag.tag);
+						tag = { uid: '', tag: new Tag() };
+					}
+				}}>Сохранить</button
+			>
 		</div>
-	{/each}
+	</div>
 </div>
