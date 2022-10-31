9e051bce8c056b6ba89baeac88d3a579c9f15d0a
Горбулёв Сергей <gorbulevsv@yandex.ru>
2022-11-01 00:07:35 +0300

    2

--- src/lib/components/TagManager.svelte
+++ src/lib/components/TagManager.svelte
@@ -2,36 +2,50 @@
 	import Tag from './Tag.svelte';
 	import { Tag as _Tag } from '$lib/models/Tag';
 	import TagSelector from './TagSelector.svelte';
+	import { onMount } from 'svelte';
 
 	export let tags = new Array();
 	export let selectedTags = new Array();
+	export let onSelect = () => {
+		tags = tags.filter((t) => !selectedTags.includes(t));
+	};
+	export let _class = '';
 	let selectedTag = new _Tag();
+
+	onMount(async () => {
+		tags = tags.filter((t) => !selectedTags.includes(t));
+	});
 </script>
 
-<div class="mt-3" style="display: grid; grid-template-columns: auto 1fr;">
+<div class={_class} style="display: grid; grid-template-columns: auto 1fr;">
 	<div class={tags.length == 0 ? 'hide' : ''}>
 		<TagSelector
 			{tags}
 			bind:selected={selectedTag}
 			onSelect={() => {
 				if (selectedTag.name != '') {
+					if (!selectedTags) selectedTags = new Array();
 					selectedTags = [...selectedTags, selectedTag];
 					tags = tags.filter((t) => t.name != selectedTag.name);
 					selectedTag = new _Tag();
+					onSelect();
 				}
 			}}
 			_class="mb-2 me-2"
 		/>
 	</div>
 	<div>
-		{#each selectedTags as item}
-			<Tag
-				{item}
-				onDelete={() => {
-					tags = [...tags, item];
-					selectedTags = selectedTags.filter((t) => t.name != item.name);
-				}}
-			/>
-		{/each}
+		{#if selectedTags}
+			{#each selectedTags as item}
+				<Tag
+					{item}
+					onDelete={() => {
+						tags = [...tags, item];
+						selectedTags = selectedTags.filter((t) => t.name != item.name);
+						onSelect();
+					}}
+				/>
+			{/each}
+		{/if}
 	</div>
 </div>
--- src/routes/admin/post/+page.svelte
+++ src/routes/admin/post/+page.svelte
@@ -6,13 +6,18 @@
 	import { onMount } from 'svelte';
 	import { goto } from '$app/navigation';
 	import Pin from '$lib/components/Pin.svelte';
+	import TagManager from '$lib/components/TagManager.svelte';
 
 	let posts = new Object();
+	$: tags = new Array();
 
 	onMount(async () => {
 		onValue(ref(db, '/posts'), (s) => {
 			if (s.exists()) posts = s.val();
 			console.log(posts);
+		});
+		onValue(ref(db, 'tags/'), (s) => {
+			if (s.exists()) tags = Object.values(s.val());
 		});
 	});
 </script>
@@ -22,51 +27,62 @@
 <div class="row">
 	{#each Object.entries(posts) as [uid, item]}
 		<PostHorizontal {uid} post={item}>
-			<div slot="adminControls" class="d-flex justify-content-end">
+			<div slot="adminControls" class="d-flex justify-content-between align-items-center">
 				<div class="d-flex me-3">
-					<Pin
-						classFontAwesome="fa-regular fa-eye"
-						text="опубликовать"
-						_class="me-2"
-						bind:checked={item.published}
-						onChange={() => {
-							update(ref(db, `/posts/${uid}`), item);
-						}}
-					/>
-					<Pin
-						classFontAwesome="fa-solid fa-thumbtack"
-						text="закрепить"
-						_class="me-2"
-						bind:checked={item.pinned}
-						onChange={() => {
-							update(ref(db, `/posts/${uid}`), item);
-						}}
-					/>
-					<Pin
-						classFontAwesome="fa-solid fa-brush"
-						text="инвертировать"
-						bind:checked={item.inverted}
-						onChange={() => {
-							update(ref(db, `/posts/${uid}`), item);
-						}}
+					<TagManager
+						{tags}
+						bind:selectedTags={item.tags}
+						onSelect={() => update(ref(db, `/posts/${uid}`), item)}
 					/>
 				</div>
-				<div class="d-flex">
-					<button class="btn btn-sm btn-light me-1" on:click={() => goto(`/admin/post/edit/${uid}`)}
-						><i class="fa-solid fa-pencil" /></button
-					>
-					<div class="dropdown dropend">
-						<button class="btn btn-sm btn-light" data-bs-toggle="dropdown"
-							><i class="fa-solid fa-trash text-danger" /></button
+				<div class="d-flex mb-2">
+					<div class="d-flex me-3">
+						<Pin
+							classFontAwesome="fa-regular fa-eye"
+							text="опубликовать"
+							_class="me-2"
+							bind:checked={item.published}
+							onChange={() => {
+								update(ref(db, `/posts/${uid}`), item);
+							}}
+						/>
+						<Pin
+							classFontAwesome="fa-solid fa-thumbtack"
+							text="закрепить"
+							_class="me-2"
+							bind:checked={item.pinned}
+							onChange={() => {
+								update(ref(db, `/posts/${uid}`), item);
+							}}
+						/>
+						<Pin
+							classFontAwesome="fa-solid fa-brush"
+							text="инвертировать"
+							bind:checked={item.inverted}
+							onChange={() => {
+								update(ref(db, `/posts/${uid}`), item);
+							}}
+						/>
+					</div>
+					<div class="d-flex">
+						<button
+							class="btn btn-sm btn-light me-1"
+							on:click={() => goto(`/admin/post/edit/${uid}`)}
+							><i class="fa-solid fa-pencil" /></button
 						>
-						<div class="dropdown-menu p-2 text-center">
-							<p>Вы действительно хотите удалить эту публикацию?</p>
-							<button class="btn btn-sm btn-light" data-bs-toggle="dropdown">Отмена</button>
-							<button
-								class="btn btn-sm btn-light text-danger"
-								data-bs-toggle="dropdown"
-								on:click={() => remove(ref(db, `/posts/${uid}`))}>Удалить</button
+						<div class="dropdown dropend">
+							<button class="btn btn-sm btn-light" data-bs-toggle="dropdown"
+								><i class="fa-solid fa-trash text-danger" /></button
 							>
+							<div class="dropdown-menu p-2 text-center">
+								<p>Вы действительно хотите удалить эту публикацию?</p>
+								<button class="btn btn-sm btn-light" data-bs-toggle="dropdown">Отмена</button>
+								<button
+									class="btn btn-sm btn-light text-danger"
+									data-bs-toggle="dropdown"
+									on:click={() => remove(ref(db, `/posts/${uid}`))}>Удалить</button
+								>
+							</div>
 						</div>
 					</div>
 				</div>
