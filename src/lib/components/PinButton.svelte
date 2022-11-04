88989049ba2cb52331290bc02d852288f2e87e63
Горбулёв Сергей <gorbulevsv@yandex.ru>
2022-11-04 23:27:14 +0300

    4

--- src/lib/components/PinButton.svelte
+++ src/lib/components/PinButton.svelte
@@ -3,13 +3,13 @@
 	export let titlePressed = 'кнопка';
 	export let pressed = false;
 	export let onPressed = () => {};
-	export let classNoPressed = 'btn-outline-secondary';
+	export let classNoPressed = 'btn-dark';
 	export let classPressed = 'btn-dark';
 	export let _class = '';
 </script>
 
 <button
-	class=" btn {pressed ? classPressed : classNoPressed} {_class}"
+	class="btn {pressed ? classPressed : classNoPressed} {_class}"
 	on:click={() => {
 		pressed = !pressed;
 		onPressed();
