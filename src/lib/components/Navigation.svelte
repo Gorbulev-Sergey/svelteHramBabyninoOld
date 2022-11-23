56fb028607418af4855985a606ddeae20ccc175d
Горбулёв Сергей <gorbulevsv@yandex.ru>
2022-11-24 00:00:43 +0300

    7

--- src/lib/components/Navigation.svelte
+++ src/lib/components/Navigation.svelte
@@ -1,82 +1,84 @@
 <script>
 	import { goto } from '$app/navigation';
 	import { page } from '$app/stores';
+	import CheckBreakepoint from './CheckBreakepoint.svelte';
 
 	export let title = 'Название';
 	export let routesLeft = new Array();
 	export let routesRight = new Array();
 	export let isAdmin = true;
-	let innerWidth = 0;
-	let breakpoint = 922;
 </script>
 
-<svelte:window bind:innerWidth />
-
 <div class="fixed-top bg-white py-2">
-	{#if innerWidth > breakpoint}
-		<div class="container-fluid d-flex justify-content-between align-items-center">
-			<div class="flex-grow-1 d-flex justify-content-start align-items-center">
+	<CheckBreakepoint>
+		<div slot="big">
+			<div class="container-fluid d-flex justify-content-between align-items-center">
+				<div class="flex-grow-1 d-flex justify-content-start align-items-center">
+					<button
+						class="btn btn-light bg-white border-0 text-uppercase me-2"
+						on:click={() => goto(routesLeft[0].url)}><b>{@html title}</b></button
+					>
+					<div>
+						{#each routesLeft as item}
+							<a
+								class="btn btn-light bg-white border-0 me-1 {item.url.split('/')[2] ==
+								$page.url.pathname.split('/')[2]
+									? 'fw-bold'
+									: ''}"
+								href={item.url}>{item.title}</a
+							>
+						{/each}
+					</div>
+				</div>
+				{#if isAdmin}
+					<div>
+						{#each routesRight as item}
+							<a class="btn btn-light bg-white border-0 me-1" href={item.url}>{item.title}</a>
+						{/each}
+					</div>
+				{/if}
+			</div>
+		</div>
+		<div slot="small">
+			<div class="container-fluid d-flex justify-content-between align-items-center dropdown">
 				<button
 					class="btn btn-light bg-white border-0 text-uppercase me-2"
 					on:click={() => goto(routesLeft[0].url)}><b>{@html title}</b></button
 				>
-				<div>
+				<button
+					class="btn btn-light bg-white border-0 text-uppercase me-2"
+					data-bs-toggle="dropdown"><i class="fa-solid fa-bars" /></button
+				>
+				<ul class="dropdown-menu border-0 rounded-0 w-100 py-2 mt-1">
 					{#each routesLeft as item}
-						<a
-							class="btn btn-light bg-white border-0 me-1 {item.url.split('/')[2] ==
-							$page.url.pathname.split('/')[2]
-								? 'fw-bold'
-								: ''}"
-							href={item.url}>{item.title}</a
-						>
-					{/each}
-				</div>
-			</div>
-			{#if isAdmin}
-				<div>
-					{#each routesRight as item}
-						<a class="btn btn-light bg-white border-0 me-1" href={item.url}>{item.title}</a>
-					{/each}
-				</div>
-			{/if}
-		</div>
-	{:else}
-		<div class="container-fluid d-flex justify-content-between align-items-center dropdown">
-			<button
-				class="btn btn-light bg-white border-0 text-uppercase me-2"
-				on:click={() => goto(routesLeft[0].url)}><b>{@html title}</b></button
-			>
-			<button class="btn btn-light bg-white border-0 text-uppercase me-2" data-bs-toggle="dropdown"
-				><i class="fa-solid fa-bars" /></button
-			>
-			<ul class="dropdown-menu border-0 rounded-0 w-100 pb-3">
-				{#each routesLeft as item}
-					<li>
-						<a
-							class="dropdown-item py-2 {item.url.split('/')[2] == $page.url.pathname.split('/')[2]
-								? 'fw-bold'
-								: ''}"
-							href={item.url}>{item.title}</a
-						>
-					</li>
-				{/each}
-				{#if isAdmin}
-					{#each routesRight as item}
 						<li>
-							<button
-								class="dropdown-item"
-								on:click={async () => {
-									goto(item.url);
-								}}
+							<a
+								class="dropdown-item ps-4 py-2 {item.url.split('/')[2] ==
+								$page.url.pathname.split('/')[2]
+									? 'fw-bold'
+									: ''}"
+								href={item.url}>{item.title}</a
 							>
-								<b class="text-uppercase text-danger">
-									{item.title}
-								</b>
-							</button>
 						</li>
 					{/each}
-				{/if}
-			</ul>
+					{#if isAdmin}
+						{#each routesRight as item}
+							<li>
+								<button
+									class="dropdown-item ps-4 py-2"
+									on:click={async () => {
+										goto(item.url);
+									}}
+								>
+									<b class="text-uppercase">
+										{item.title}
+									</b>
+								</button>
+							</li>
+						{/each}
+					{/if}
+				</ul>
+			</div>
 		</div>
-	{/if}
+	</CheckBreakepoint>
 </div>
--- src/routes/guest/+layout.svelte
+++ src/routes/guest/+layout.svelte
@@ -26,7 +26,11 @@
 	];
 </script>
 
-<Navigation title="<span class='text-danger'>Храм</span> ВОЗНЕСЕНИЯ" {routesLeft} {routesRight} />
+<Navigation
+	title="<span class='text-danger'>Храм</span> ВОЗНЕСЕНИЯ ГОСПОДНЯ"
+	{routesLeft}
+	{routesRight}
+/>
 <div class="container" style="margin-top:4.5em; margin-bottom:3em; min-height:60vh;">
 	<slot />
 </div>
