61dbccc2ae74daf391cf92df60b06a3463cb4fea
Горбулёв Сергей <gorbulevsv@yandex.ru>
2022-12-06 21:02:32 +0300

    7

--- src/lib/components/PageTitleWrap.svelte
+++ src/lib/components/PageTitleWrap.svelte
@@ -5,7 +5,7 @@
 
 <div class="d-flex justify-content-between flex-wrap align-items-center mb-3 no-print {_class}">
 	<div class="d-flex justify-content-between align-items-center flex-wrap">
-		<h4 class="text-dark my-0 me-3 text-nowrap">{title}</h4>
+		<h4 class="text-dark ms-1 my-0 me-3 text-nowrap">{title}</h4>
 		<div class="my-2 my-md-0">
 			<slot name="center" />
 		</div>
