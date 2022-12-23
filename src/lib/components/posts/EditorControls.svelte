a1c396fc5767140dc44d7ea08b0218c8f86ceb8f
Горбулёв Сергей <gorbulevsv@yandex.ru>
2022-12-24 00:44:51 +0300

    1

--- src/lib/components/posts/EditorControls.svelte
+++ src/lib/components/posts/EditorControls.svelte
@@ -107,6 +107,116 @@
 				</div>
 			</div>
 		</div>
+		<!--Размер шрифтов-->
+		<div class="btn-group btn-group-sm me-1">
+			<div class="dropdown d-inline">
+				<button
+					class="btn btn-sm btn-primary text-dark dropdown-toggle"
+					data-bs-toggle="dropdown"
+					title="Размер шрифта"
+				>
+					14 pt
+				</button>
+				<div class="dropdown-menu">
+					<button class="dropdown-item" on:click={() => document.execCommand('fontSize', false, 1)}
+						>10 pt</button
+					>
+					<button class="dropdown-item" on:click={() => document.execCommand('fontSize', false, 2)}
+						>12 pt</button
+					>
+					<button class="dropdown-item" on:click={() => document.execCommand('fontSize', false, 3)}
+						>14 pt</button
+					>
+					<button class="dropdown-item" on:click={() => document.execCommand('fontSize', false, 4)}
+						>16 pt</button
+					>
+					<button class="dropdown-item" on:click={() => document.execCommand('fontSize', false, 5)}
+						>18 pt</button
+					>
+					<button class="dropdown-item" on:click={() => document.execCommand('fontSize', false, 6)}
+						>22 pt</button
+					>
+					<button class="dropdown-item" on:click={() => document.execCommand('fontSize', false, 7)}
+						>24 pt</button
+					>
+				</div>
+			</div>
+		</div>
+		<!--Жирный, курсив, подчёркнутый-->
+		<div class="btn-group btn-group-sm me-1">
+			<button
+				class="btn btn-primary text-dark"
+				title="Жирный"
+				on:click={() => document.execCommand('bold')}><i class="fas fa-bold" /></button
+			>
+			<button
+				class="btn btn-primary text-dark"
+				title="Курсив"
+				on:click={() => document.execCommand('italic')}><i class="fas fa-italic" /></button
+			>
+			<button
+				class="btn btn-primary text-dark"
+				title="Подчёркнутый"
+				on:click={() => document.execCommand('underline')}><i class="fas fa-underline" /></button
+			>
+		</div>
+		<!--Выравнивание текста-->
+		<div class="btn-group btn-group-sm me-1" data-bs-toggle="buttons">
+			<label class="btn btn-primary text-dark active" title="Слева">
+				<input
+					type="radio"
+					name="options"
+					id="option1"
+					autocomplete="off"
+					checked
+					on:click={() => document.execCommand('justifyLeft')}
+				/>
+				<i class="fas fa-align-left" />
+			</label>
+			<label class="btn btn-primary text-dark" title="По центру">
+				<input
+					type="radio"
+					name="options"
+					id="option2"
+					autocomplete="off"
+					on:click={() => document.execCommand('justifyCenter')}
+				/>
+				<i class="fas fa-align-center" />
+			</label>
+			<label class="btn btn-primary text-dark" title="Справа">
+				<input
+					type="radio"
+					name="options"
+					id="option3"
+					autocomplete="off"
+					on:click={() => document.execCommand('justifyRight')}
+				/>
+				<i class="fas fa-align-right" />
+			</label>
+			<label class="btn btn-primary text-dark" title="По ширине">
+				<input
+					type="radio"
+					name="options"
+					id="option4"
+					autocomplete="off"
+					on:click={() => document.execCommand('justifyFull')}
+				/>
+				<i class="fas fa-align-justify" />
+			</label>
+		</div>
+		<!--Списки-->
+		<div class="btn-group btn-group-sm me-1">
+			<button
+				class="btn btn-primary text-dark"
+				on:click={() => document.execCommand('insertOrderedList')}
+				><i class="fas fa-list-ol" /></button
+			>
+			<button
+				class="btn btn-primary text-dark"
+				on:click={() => document.execCommand('insertUnorderedList')}
+				><i class="fas fa-list-ul" /></button
+			>
+		</div>
 	{/if}
 
 	<div class="btn-group btn-group-sm">
