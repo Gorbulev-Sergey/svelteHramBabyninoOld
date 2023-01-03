fa6520167dc9770d8e8b01acc18fa57eb7b15dfa
Горбулёв Сергей <gorbulevsv@yandex.ru>
2023-01-03 19:01:54 +0300

    4

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,7 @@
 <script>
 	// @ts-nocheck
 	import { goto } from '$app/navigation';
-	import Seo from '$lib/components/Seo.svelte';
+	import SEO from '$lib/components/SEO.svelte';
 	import { onMount } from 'svelte';
 
 	onMount(() => {
@@ -9,4 +9,4 @@
 	});
 </script>
 
-<Seo title="Публикации" description="Объявления, новости, видео, статьи из жизни нашего храма" />
+<SEO title="Публикации" description="Объявления, новости, видео, статьи из жизни нашего храма" />
--- src/routes/guest/posts/+page.svelte
+++ src/routes/guest/posts/+page.svelte
@@ -1,11 +1,11 @@
 <script>
 	import { onMount } from 'svelte';
 	import { goto } from '$app/navigation';
-	import Seo from '$lib/components/Seo.svelte';
+	import SEO from '$lib/components/SEO.svelte';
 
 	onMount(async () => {
 		goto('/guest/posts/объявления');
 	});
 </script>
 
-<Seo title="Публикации" description="Объявления, новости, видео, статьи из жизни нашего храма" />
+<SEO title="Публикации" description="Объявления, новости, видео, статьи из жизни нашего храма" />
