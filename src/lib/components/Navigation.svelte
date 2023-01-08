8270e4f2d7864ba36e8cf3283b8c9e1b8b03dffd
Горбулёв Сергей <gorbulevsv@yandex.ru>
2023-01-08 21:16:24 +0300

    1

--- src/lib/components/Navigation.svelte
+++ src/lib/components/Navigation.svelte
@@ -9,10 +9,10 @@
 	export let title = 'Название';
 	export let routesLeft = new Array();
 	export let routesRight = new Array();
-	function changeTheme() {
+	let changeTheme = () => {
 		$theme = $theme == 'light' ? 'dark' : 'light';
 		localStorage.setItem('theme', $theme);
-	}
+	};
 </script>
 
 <div class="fixed-top bg-light text-dark no-print py-2">
@@ -21,17 +21,14 @@
 			<div class="flex-grow-1 d-flex justify-content-start align-items-center">
 				<button
 					class="btn btn-light bg-transparent text-dark border-0 text-uppercase me-2"
-					on:click={() => goto(routesLeft[0].url)}><b>{@html title}</b></button
-				>
+					on:click={() => goto(routesLeft[0].url)}><b>{@html title}</b></button>
 				<div>
 					{#each routesLeft as item}
 						<a
-							class="btn btn-light text-dark border-0 me-1 {item.url.split('/')[2] ==
-							$page.url.pathname.split('/')[2]
+							class="btn btn-light text-dark border-0 me-1 {item.url.split('/')[2] == $page.url.pathname.split('/')[2]
 								? 'fw-bold'
 								: ''}"
-							href={item.url}>{item.title}</a
-						>
+							href={item.url}>{item.title}</a>
 					{/each}
 				</div>
 			</div>
@@ -43,8 +40,7 @@
 				</div>
 			</IsAuth>
 			<button class="btn btn-light text-dark border-0" on:click={async () => changeTheme()}
-				><i class="fa-regular fa-sun" /></button
-			>
+				><i class="fa-regular fa-sun" /></button>
 		</div>
 	</AfterBreakpoint>
 	<BeforeBreakpoint>
@@ -54,24 +50,20 @@
 					class="btn btn-light text-dark bg-transparent border-0 text-uppercase ps-1 me-1"
 					on:click={() => goto(routesLeft[0].url)}><b>{@html title}</b></button
 				><button class="btn btn-light text-dark border-0" on:click={async () => changeTheme()}
-					><i class="fa-regular fa-sun" /></button
-				>
+					><i class="fa-regular fa-sun" /></button>
 			</div>
 			<button class="btn btn-light text-dark border-0 text-uppercase" data-bs-toggle="dropdown"
-				><i class="fa-solid fa-bars" /></button
-			>
+				><i class="fa-solid fa-bars" /></button>
 
 			<div class="dropdown-menu border-0 rounded-0 w-100 bg-light text-dark shadow-sm">
 				{#each routesLeft as item}
 					<div class="bg-light text-dark">
 						<a
-							class="btn btn-light text-dark w-100 text-start rounded-0 ps-3 {item.url.split(
-								'/'
-							)[2] == $page.url.pathname.split('/')[2]
+							class="btn btn-light text-dark w-100 text-start rounded-0 ps-3 {item.url.split('/')[2] ==
+							$page.url.pathname.split('/')[2]
 								? 'fw-bold'
 								: ''}"
-							href={item.url}>{item.title}</a
-						>
+							href={item.url}>{item.title}</a>
 					</div>
 				{/each}
 				<IsAuth>
@@ -81,8 +73,7 @@
 								class="btn btn-light text-dark w-100 text-start rounded-0 ps-3"
 								on:click={async () => {
 									goto(item.url);
-								}}
-							>
+								}}>
 								<b class="text-uppercase">
 									{item.title}
 								</b>
