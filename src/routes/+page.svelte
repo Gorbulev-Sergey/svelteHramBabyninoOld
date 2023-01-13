944c03a7e5a2767e9b2813d659667371fec69a74
Горбулёв Сергей <gorbulevsv@yandex.ru>
2023-01-13 23:39:15 +0300

    Seo

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,12 @@
 <script>
 	// @ts-nocheck
 	import { goto } from '$app/navigation';
+	import Seo from '$lib/components/SEO.svelte';
 	import { onMount } from 'svelte';
 
 	onMount(() => {
 		goto('/guest/posts');
 	});
 </script>
+
+<Seo />
