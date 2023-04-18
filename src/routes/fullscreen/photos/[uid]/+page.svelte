a5beefa9f487c7e4f596e1dc6776bbaa1c2de9e3
Горбулёв Сергей <gorbulevsv@yandex.ru>
2023-04-18 14:27:11 +0300

    10

--- src/routes/fullscreen/photos/[uid]/+page.svelte
+++ src/routes/fullscreen/photos/[uid]/+page.svelte
@@ -8,6 +8,7 @@
 	import { onMount } from 'svelte';
 
 	let album = new Album();
+	let activeImage = 0;
 
 	document.documentElement.requestFullscreen();
 	document.onfullscreenchange = e => {
@@ -39,59 +40,59 @@
 		onValue(ref(db, `/photos/${$page.params.uid}`), s => {
 			if (s.exists()) {
 				album = s.val();
+				document.querySelector('#carouselExample').addEventListener('slide.bs.carousel', function (e) {
+					activeImage = e.to;
+				});
 			}
 		});
 	});
 </script>
 
-{#if album.photos.length > 0}
-	<button
-		class="btn btn-light text-dark bg-light border-0 bg-opacity-75 position-absolute m-3"
-		style="z-index: 1000; right:0; top:0"
-		title="Закрыть"
-		on:click={goBack}>
-		<i class="fa-solid fa-xmark" />
-	</button>
-	<!-- svelte-ignore a11y-autofocus -->
-	<div id="carouselExample" class="carousel slide" data-bs-ride="false" autofocus>
-		{#if album.photos.length > 1}
-			<div class="carousel-indicators">
-				{#each album.photos as photo, i}
-					<button data-bs-target="#carouselExample" class={i == 0 ? 'active' : ''} data-bs-slide-to={i.toString()} />
-				{/each}
-			</div>
-		{/if}
-		<div class="carousel-inner">
+<button
+	class="btn btn-light text-dark bg-light border-0 bg-opacity-75 position-absolute m-3"
+	style="z-index: 1000; left:0; top:0"
+	title="Скачать фотографию"
+	on:click={downloadImage(album.photos[activeImage].url, activeImage)}>
+	<i class="fa-solid fa-download" />
+</button>
+<button
+	class="btn btn-light text-dark bg-light border-0 bg-opacity-75 position-absolute m-3"
+	style="z-index: 1000; right:0; top:0"
+	title="Закрыть"
+	on:click={goBack}>
+	<i class="fa-solid fa-xmark" />
+</button>
+<div id="carouselExample" class="carousel slide" data-bs-ride="false">
+	{#if album.photos.length > 1}
+		<div class="carousel-indicators">
 			{#each album.photos as photo, i}
-				<div class="carousel-item {i == 0 ? 'active' : ''}">
-					<button
-						class="btn btn-light text-dark bg-light border-0 bg-opacity-75 position-absolute m-3"
-						style="z-index: 1000; left:0; top:0"
-						title="Скачать фотографию"
-						on:click={downloadImage(photo.url, i)}>
-						<i class="fa-solid fa-download" />
-					</button>
-					<div
-						class="img-fluid h-100"
-						style="background-image: url({photo.url});
-							background-repeat: no-repeat; background-position: center; background-size: contain; min-height:100vh;" />
-					{#if photo.title}
-						<div class="carousel-caption d-none d-md-block mb-2">
-							{photo.title}
-						</div>
-					{/if}
-				</div>
+				<button data-bs-target="#carouselExample" class={i == 0 ? 'active' : ''} data-bs-slide-to={i.toString()} />
 			{/each}
 		</div>
-		{#if album.photos.length > 1}
-			<button class="carousel-control-prev" data-bs-target="#carouselExample" data-bs-slide="prev">
-				<span class="carousel-control-prev-icon" />
-				<span class="visually-hidden">Предыдущий</span>
-			</button>
-			<button class="carousel-control-next" data-bs-target="#carouselExample" data-bs-slide="next">
-				<span class="carousel-control-next-icon" />
-				<span class="visually-hidden">Следующий</span>
-			</button>
-		{/if}
+	{/if}
+	<div class="carousel-inner">
+		{#each album.photos as photo, i}
+			<div class="carousel-item {i == 0 ? 'active' : ''}">
+				<div
+					class="img-fluid h-100"
+					style="background-image: url({photo.url});
+							background-repeat: no-repeat; background-position: center; background-size: contain; min-height:100vh;" />
+				{#if photo.title}
+					<div class="carousel-caption d-none d-md-block mb-2">
+						{photo.title}
+					</div>
+				{/if}
+			</div>
+		{/each}
 	</div>
-{/if}
+	{#if album.photos.length > 1}
+		<button class="carousel-control-prev" data-bs-target="#carouselExample" data-bs-slide="prev">
+			<span class="carousel-control-prev-icon" />
+			<span class="visually-hidden">Предыдущий</span>
+		</button>
+		<button class="carousel-control-next" data-bs-target="#carouselExample" data-bs-slide="next">
+			<span class="carousel-control-next-icon" />
+			<span class="visually-hidden">Следующий</span>
+		</button>
+	{/if}
+</div>
